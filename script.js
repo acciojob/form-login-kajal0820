ac9195b90cf1ec235b84b1b0593775d16bf75836
kajal0820 <130426994+kajal0820@users.noreply.github.com>
2025-05-16 11:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 function getFormvalue() {
-	event.preventDefault(); // Prevent page reload
-
-      const fname = document.forms["userForm"]["fname"].value.trim();
-      const lname = document.forms["userForm"]["lname"].value.trim();
-
-      if (!fname && !lname) {
-        alert("Please enter your first and last name.");
-        return;
-      }
-
-      alert(`${fname} ${lname}`);
-    }
-  </script>
-
     //Write your code here
 
 }
